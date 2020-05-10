10c3761f0f08e41466e8b53d1813907fc2ec3716
Pezanne Khambatta <pkhambat@risd.edu>
2020-05-10 13:49:33 -0700

    visualization works with large file

--- js/line-chart.js
+++ js/line-chart.js
@@ -82,4 +82,4 @@ var plotLine = (countyId) => {
         g.selectAll('*').remove();
     }
     
-    d3.csv('data/test.csv', wrangle).then(data => gData = data);
+    d3.csv('https://raw.githubusercontent.com/usf-cs360-spring2020/project-pkhambat1/gh-pages/data/_County_Zhvi_AllHomes.csv', wrangle).then(data => gData = data);
